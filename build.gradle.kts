8631abb8827114739c58f3d40b1035c24e969d56
Vyacheslav Peshne <Vyacheslav.Peshne@magenta-technology.com>
2021-02-13 04:26:05 +0400

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     java
+    id("io.qameta.allure") version "2.8.1"
 }
 
 group = "org.example"
@@ -12,8 +13,18 @@ repositories {
 dependencies {
     testImplementation("junit", "junit", "4.13.1")
     testImplementation("com.codeborne", "selenide", "5.15.1")
+    implementation("io.qameta.allure:allure-junit4:2.13.6")
+    implementation("io.qameta.allure:allure-selenide:2.13.6")
+
+    runtimeOnly("org.slf4j:slf4j-simple:1.7.30")
 }
 
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
 }
+
+test {
+    useJUnit()
+}
+
+apply(java);
--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -1,24 +1,40 @@
+import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
 import static org.junit.Assert.assertNotNull;
 
 public class GoogleTest {
 
+    @Before
+    public void setUp() {
+        SelenideLogger.addListener("AllureSelenide",
+                new AllureSelenide()
+                .screenshots(true)
+                .savePageSource(true));
+    }
+
     @Test
-    public void hello() {
+    public void searchTest() {
         //open("https://google.com");
+        Configuration.browser = "edge";
         open("https://duckduckgo.com");
         $(By.name("q"))
                 .setValue("selenide heisenbug")
                 .pressEnter();
 
-        $(".active")
-                .shouldBe(Condition.visible)
-                .shouldHave(Condition.text("hello"));
-
-        sleep(5000);
+        $$(".results_links_deep").shouldHave(CollectionCondition.sizeGreaterThan(5));
+      /*  $(".active")
+                .shouldBe(visible)
+                .shouldHave(text("hello"));
+*/
+        sleep(4000);
     }
 }
